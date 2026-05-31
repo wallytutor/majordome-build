613d923e34da738831093877ddcab8420427a1d7
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-05-31 19:55:45 +0200

    Preparing future feature

---
+++ src/majordome_build/new_project.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# .
+# ├── Cargo.toml
+# ├── LICENSE
+# ├── README.md
+# ├── pyproject.toml
+# └── src
+#     ├── lib.rs
+#     ├── majordome_sample
+#     │   ├── __init__.py
+#     │   └── py.typed
+#     └── prelude.rs
+
+def main():
+    print("Work in progress")
+
+
+CARGO_TOML = ""
+
+LICENSE = ""
+
+README = ""
+
+PYPROJECT = ""
